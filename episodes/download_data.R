9a87d2aefa51445368410653e3de5a7c256495a9
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-08-02 18:15:17 +0100

    add command to create data dir; add conditional to download data files only once

--- episodes/download_data.R
+++ episodes/download_data.R
@@ -1,1 +1,6 @@
-download.file(url = "https://github.com/Bioconductor/bioconductor-teaching/raw/master/data/UniProt/uniprot-filtered-reviewed_human_96.fasta.gz", destfile = "data/uniprot-filtered-reviewed_human_96.fasta.gz")
+if (!file.exists("data/uniprot-filtered-reviewed_human_96.fasta.gz")) {
+    dir.create("data", showWarnings = FALSE)
+    download.file(
+        url = "https://github.com/Bioconductor/bioconductor-teaching/raw/master/data/UniProt/uniprot-filtered-reviewed_human_96.fasta.gz",
+        destfile = "data/uniprot-filtered-reviewed_human_96.fasta.gz")
+}
