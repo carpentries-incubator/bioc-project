5500af6161544bafde2982b1c9df812224ef0fff
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-04-28 12:02:02 +0100

    Extend lesson on genomic ranges (#45)
    * rephrase
    
    * demo mcols() and names()
    
    * demonstrate start() end() width()
    
    * rephrase
    
    * rename demo_with_metadata
    
    * rephrase
    
    * rephrase
    
    * replace interval by range
    
    * fix fonts in SVG files
    
    * demonstrate indexing on iranges
    
    * fix emdash
    
    * rephrase
    
    * parenthesis
    
    * genomic ranges not intervals
    
    * develop figure caption
    
    * split demo of indexing by integer and name
    
    * rephrase
    
    * tense
    
    * rephrase
    
    * develop the going further section
    
    * rephrase
    
    * rephrase
    
    * rephrase
    
    * demonstrate seqinfo
    
    * demonstrate import of gtf file into granges
    
    * explain rtracklayer import
    
    * demonstrate method subset on GRanges
    
    * demonstrate the split method and lengths of grangeslist
    
    * change code font in all illustrations
    
    * rephrase
    
    * code font
    
    * rephrase
    
    * describe the result of split()
    
    * extend
    
    * update section title
    
    * fix object name
    
    * weight statement
    
    * mention Ensembl and UCSC
    
    * emdash
    
    * rephrase
    
    * add challenge
    
    * demonstrate subset by overlap
    
    * going further
    
    * white space
    
    * replace let us by we

--- episodes/download_data.R
+++ episodes/download_data.R
@@ -4,3 +4,7 @@ download.file(
     url = "https://raw.githubusercontent.com/Bioconductor/bioconductor-teaching/master/data/TrimmomaticAdapters/TruSeq3-PE-2.fa", 
     destfile = "data/TruSeq3-PE-2.fa"
 )
+download.file(
+    url = "https://raw.githubusercontent.com/Bioconductor/bioconductor-teaching/master/data/ActbGtf/actb.gtf", 
+    destfile = "data/actb.gtf"
+)
