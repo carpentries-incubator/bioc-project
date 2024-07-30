c6a57a25b2b3a8f1d40f9b8196ce6914c2877808
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2024-07-30 13:24:03 +0100

    update renv

--- renv/activate.R
+++ renv/activate.R
@@ -6,9 +6,7 @@ local({
   attr(version, "sha") <- NULL
 
   # the project directory
-  project <- Sys.getenv("RENV_PROJECT")
-  if (!nzchar(project))
-    project <- getwd()
+  project <- getwd()
 
   # use start-up diagnostics if enabled
   diagnostics <- Sys.getenv("RENV_STARTUP_DIAGNOSTICS", unset = "FALSE")
@@ -128,21 +126,6 @@ local({
   
     tail <- paste(rep.int(suffix, n), collapse = "")
     paste0(prefix, " ", label, " ", tail)
-  
-  }
-  
-  heredoc <- function(text, leave = 0) {
-  
-    # remove leading, trailing whitespace
-    trimmed <- gsub("^\\s*\\n|\\n\\s*$", "", text)
-  
-    # split into lines
-    lines <- strsplit(trimmed, "\n", fixed = TRUE)[[1L]]
-  
-    # compute common indent
-    indent <- regexpr("[^[:space:]]", lines)
-    common <- min(setdiff(indent, -1L)) - leave
-    paste(substring(lines, common), collapse = "\n")
   
   }
   
@@ -648,9 +631,6 @@ local({
   
     # if the user has requested an automatic prefix, generate it
     auto <- Sys.getenv("RENV_PATHS_PREFIX_AUTO", unset = NA)
-    if (is.na(auto) && getRversion() >= "4.4.0")
-      auto <- "TRUE"
-  
     if (auto %in% c("TRUE", "True", "true", "1"))
       return(renv_bootstrap_platform_prefix_auto())
   
@@ -842,23 +822,24 @@ local({
   
     # the loaded version of renv doesn't match the requested version;
     # give the user instructions on how to proceed
-    dev <- identical(description[["RemoteType"]], "github")
-    remote <- if (dev)
+    remote <- if (!is.null(description[["RemoteSha"]])) {
       paste("rstudio/renv", description[["RemoteSha"]], sep = "@")
-    else
+    } else {
       paste("renv", description[["Version"]], sep = "@")
+    }
   
     # display both loaded version + sha if available
     friendly <- renv_bootstrap_version_friendly(
       version = description[["Version"]],
-      sha     = if (dev) description[["RemoteSha"]]
+      sha     = description[["RemoteSha"]]
     )
   
-    fmt <- heredoc("
-      renv %1$s was loaded from project library, but this project is configured to use renv %2$s.
-      - Use `renv::record(\"%3$s\")` to record renv %1$s in the lockfile.
-      - Use `renv::restore(packages = \"renv\")` to install renv %2$s into the project library.
-    ")
+    fmt <- paste(
+      "renv %1$s was loaded from project library, but this project is configured to use renv %2$s.",
+      "- Use `renv::record(\"%3$s\")` to record renv %1$s in the lockfile.",
+      "- Use `renv::restore(packages = \"renv\")` to install renv %2$s into the project library.",
+      sep = "\n"
+    )
     catf(fmt, friendly, renv_bootstrap_version_friendly(version), remote)
   
     FALSE
